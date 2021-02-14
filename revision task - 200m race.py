4886a4f323e69bd4e79f2f221ae7482deaebb3b0
ellarendell <65571660+ellarendell@users.noreply.github.com>
2021-02-14 17:31:27 +1300

    v3
    getting the max finish time

--- revision task - 200m race.py
+++ revision task - 200m race.py
@@ -3,7 +3,22 @@
 #Revision Tasks
 #200 Metre Race
 
-finish_time = 0
-while finish_time != -1:
+
+times_list = [] #used to make calculations for average and max times
+#while True loop so the -1 is not accounted for in the times_list
+while True:
     finish_time = float(input("What is the finsihing time of the 200m race (in secs): "))
-print("blas")
+    if finish_time != -1:
+        times_list.append(finish_time)
+    elif finish_time == -1:
+        break
+
+max_time = 0
+#each finish time is compared
+for time in times_list:
+    #getting the largest time
+    if time > max_time:
+        max_time = time
+print(max_time)
+
+    
