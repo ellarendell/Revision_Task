ddbec6f43a1c2a674ab2134c2216ddee33e225e5
ellarendell <65571660+ellarendell@users.noreply.github.com>
2021-02-14 17:17:38 +1300

    v2
    getting the max time of the finish times

---
+++ revision task - 200m race.py
@@ -0,0 +1,9 @@
+#Ella Rendell
+#14.02.21
+#Revision Tasks
+#200 Metre Race
+
+finish_time = 0
+while finish_time != -1:
+    finish_time = float(input("What is the finsihing time of the 200m race (in secs): "))
+print("blas")
