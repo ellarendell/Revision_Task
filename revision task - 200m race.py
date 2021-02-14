c1f6bc0f7292c8f8dc7b0937c193e7ed1c30c29d
ellarendell <65571660+ellarendell@users.noreply.github.com>
2021-02-14 17:46:23 +1300

    v5
    final version - calculating average time and final output

--- revision task - 200m race.py
+++ revision task - 200m race.py
@@ -13,16 +13,21 @@ while True:
     elif finish_time == -1:
         break
 
-max_time = 0
+min_time = float('inf') #represents infinity
 #each finish time is compared
 for time in times_list:
     #getting the largest time
-    if time > max_time:
-        max_time = time
-print(max_time,"max time")
+    if time < min_time:
+        min_time = time
 
 #all of the race times together to calculate average
 total_times = 0
 for time in times_list:
     total_times += time
-print(total_times,"total time")
+
+#calculating average race time
+average_time = total_times / len(times_list)
+
+#output
+print("Average race time for 200m race: ",average_time)
+print("Fastest race time for 200m race: ",min_time)
